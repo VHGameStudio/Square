accea1d02dec79c8bbd9cd6c010836a7c588a5ef
vixorem <priorityvctr@gmail.com>
2019-02-21 23:40:47 +1000

    bugfix

--- core/src/com/square/actors/Border.java
+++ core/src/com/square/actors/Border.java
@@ -3,7 +3,6 @@ package com.square.actors;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.square.box2d.BorderUserData;
-import com.square.box2d.CircleUserData;
 
 public class Border extends GameActor {
 
